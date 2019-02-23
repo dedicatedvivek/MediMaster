e2e408d9d7002d607099c715c936041f066ecfc6
dedicatedvivek <vivekiyer98@gmail.com>
2019-02-23 13:52:45 +0530

    PatientFormInitialized

---
+++ resources/views/new_patient.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Patient Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+
+  
+</head>
+
+<body>
+  
+
+
+<div class="form-container" id="form-container">
+  
+    <h1 class="form-header">Create Your Profile Now!!!</h1>
+   
+    <form class="signup-form" method="post" action="/insert_patient">
+      <label for="signup-name">Your Name</label>
+      <input id="signup-name" type="text" name="name" autocomplete="off"/>
+
+      <label for="signup-email">Email Address</label>
+      <input id="signup-email" type="email" name="email" autocomplete="off"/>
+      
+      <label for="signup-pw">Password</label>
+      <input id="signup-pw" type="password" name="pw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <label for="signup-cpw">Confirm Password</label>
+      <input id="signup-cpw" type="password" name="cpw" autocomplete="off"/>
+      <button class="btn btn-outline submit-btn"><span>signup</span></button>
+    </form>
+  </div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/indexpatientform.js"></script>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/new_patient', function () {
+    return view('new_patient');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
